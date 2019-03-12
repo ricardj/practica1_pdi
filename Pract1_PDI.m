4fd3931392e5e0e436bef0d229f8131c814e6d00
ricardj <43671359+ricardj@users.noreply.github.com>
2019-03-12 16:57:18 +0100

    Practical started

--- Pract1_PDI.m
+++ Pract1_PDI.m
@@ -11,7 +11,99 @@ close all;
 %Read the input image
 [img_filename, img_pathname] = uigetfile({'*.jpg';'*.jpeg';'*.png'},'Choose an image');
 img = imread(strcat(img_pathname,img_filename));
+imshow(img);
+
+%Input dialog
+answer = inputdlg("Enter the number of ROI's", "ROI chooser");
+roiNumber = str2double(answer(1));
+%msgbox("Use the mouse to draw a polygon to select the " + roiNumber + " ROI(s).","ROI settings");
+roiNumber = uint8(roiNumber);
+
+%Once we get the image we take it to chromatic values
+for arrayIndex = 1:roiNumber
+    h = imrect;
+    mask = h.createMask();
+    mask = im2uint8(mask)/255;
+    newImage = img.*mask;
+    rectPosition = getPosition(h)
+    %rectSize = size(mask)
+    croppedImage = imcrop(newImage, rectPosition);
+    croppedImages{arrayIndex} = croppedImage;
+end
+close all;
+
+columns = 2;
+rows = 1;
+for index = 1:roiNumber
+    figure(double(index));
+    subplot(rows,columns,1);
+    imshow(croppedImages{1});
+    title("ROI - Original Image");
+    subplot(rows,columns,2);
+    imhist(croppedImages{1});
+    title("Image Histogram.");
+    position  = ginput(1);
+    %TODO: normalizar para mejor tresholding
+    bynarizedImage = rgb2gray(croppedImages{index});
+    bynarizedImage(bynarizedImage <= position(1)) = 0;
+    bynarizedImage(bynarizedImage > position(1)) = 255;
+    bynarizedImages{index} = bynarizedImage;
+end
+close all;
+
+columns = 1;
+rows = 2;
+
+for index = 1:roiNumber
+    figure(double(index));
+    subplot(rows,columns,1);
+    imshow(croppedImages{index});
+    title("Original image");
+    subplot(rows,columns,2);
+    imshow(bynarizedImages{index});
+    title("Bynary mask");
+end
+
+%We create a list dialog with the operations
+operationList = {
+    "Dilation",
+    "Erosion",
+    "Closing",
+    "Opening",
+    "Skeletonitzation"
+    };
+strelList = {
+ "Square",
+ "Horizontal",
+ "Vertical"
+}
 
 
+option = "Yes";
+while strcmp(option,"Yes")
+    [operation,okOperation] = listdlg('ListString',operationList);
 
+    switch operationList{operation}
+        case "Dilation"
+            
+        case "Erosion"
+        case "Closing"
+        case "Opening"
+        case "Skeletonitzation"
+        otherwise
+            disp("Error: Operation not finded");
+    end
+    
+    for index = 1:roiNumber
+        figure(double(index));
+        subplot(rows,columns,1);
+        imshow(croppedImages{index});
+        title("Original image");
+        subplot(rows,columns,2);
+        imshow(bynarizedImages{index});
+        title("Bynary mask");
+    end
+    
+    option = questdlg("Do you want to continue applying operations?","Operation checker","Yes","No","Yes");
+end
 
